950cdd86dbefa2bc885f82b8011c860b099d0bfd
MakerSquare Student <students@themakersquare.com>
2013-11-26 16:04:58 -0600

    Add multiple features, and DRY up the code on the instance of Twitter::REST::Client.new

--- follower_clash.rb
+++ follower_clash.rb
@@ -2,3 +2,20 @@ require 'rubygems'
 require 'bundler/setup'
 
 require_relative "lib/user_comparer"
+
+user1 = UserComparer::User.new('ornellasmike')
+user2 = UserComparer::User.new('drumwagon')
+comparer = UserComparer::Comparer.new(user1, user2)
+puts "#{user1.username}'s full name is: #{user1.fullName}"
+puts "#{user2.username}'s full name is: #{user2.fullName}"
+puts "#{user1.username}'s follower count: #{user1.followers}"
+puts "#{user2.username}'s follower count: #{user2.followers}"
+puts "And the user with the most followers is: #{comparer.compare}"
+puts "#{user1.username}'s friend count: #{user1.friends}"
+puts "#{user2.username}'s friend count: #{user2.friends}"
+puts "And the user with the most friends is: #{comparer.following}"
+puts "#{user1.username}'s tweet count: #{user1.tweets}"
+puts "#{user2.username}'s tweet count: #{user2.tweets}"
+puts "And the user with the most tweets is: #{comparer.tweets}"
+puts "#{user1.username}'s last tweet was: #{user1.last_tweet}"
+puts "#{user2.username}'s last tweet was: #{user2.last_tweet}"
--- lib/user_comparer.rb
+++ lib/user_comparer.rb
@@ -7,15 +7,27 @@ module UserComparer
 		attr_reader :username
 		def initialize(username)
 		  @username = username
-		end
-		def followers
-		  client = Twitter::REST::Client.new do |config|
+		  @client = Twitter::REST::Client.new do |config|
 			  config.consumer_key       = ENV['CONSUMER_KEY']
 			  config.consumer_secret    = ENV['CONSUMER_SECRET']
 			  config.access_token        = ENV['OAUTH_TOKEN']
 			  config.access_token_secret = ENV['OAUTH_TOKEN_SECRET']
 			end
-		client.user(@username).followers_count
+		end
+		def followers
+			@client.user(@username).followers_count
+		end
+		def friends
+		 	@client.user(@username).friends_count
+		end
+		def tweets
+			@client.user(@username).tweets_count
+		end
+		def last_tweet
+			@client.user(@username).status.text
+		end
+		def fullName
+			@client.user(@username).name
 		end
   end
 
@@ -25,6 +37,31 @@ module UserComparer
 		  @user2 = user2
 		end
 		def compare
+			if @user1.followers > @user2.followers
+				return @user1.username
+			elsif @user2.followers > @user1.followers
+				return @user2.username
+			else
+				return "Both users have the same number of followers"
+			end
+		end
+		def following
+			if @user1.friends > @user2.friends
+				return @user1.username
+			elsif @user2.friends > @user1.friends
+				return @user2.username
+			else
+				return "Both users have the same number of friends"
+			end
+		end
+		def tweets
+			if @user1.tweets > @user2.tweets
+				return @user1.username
+			elsif @user2.tweets > @user1.tweets
+				return @user2.username
+			else
+				return "Both users have the same number of tweets"
+			end
 		end
   end
 end
